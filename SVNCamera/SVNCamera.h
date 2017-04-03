0caac3ea6bfd66f35aa2ed0bf372865add15da7d
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-03 16:00:53 -0700

    Initial Commit

---
+++ SVNCamera/SVNCamera.h
@@ -0,0 +1,19 @@
+//
+//  SVNCamera.h
+//  SVNCamera
+//
+//  Created by Aaron Dean Bikis on 4/3/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVNCamera.
+FOUNDATION_EXPORT double SVNCameraVersionNumber;
+
+//! Project version string for SVNCamera.
+FOUNDATION_EXPORT const unsigned char SVNCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVNCamera/PublicHeader.h>
+
+
